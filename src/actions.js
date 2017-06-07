5515846f4021338765effc368368ae5161a516c3
Sara <sara.szajewska@gmail.com>
2017-06-07 13:00:21 +0200

    message

---
+++ src/actions.js
@@ -0,0 +1,36 @@
+import "./actions.js";
+import "./reducer.js";
+
+let initialState = {
+  accountNo: 1001,
+  balance: null,
+  destinationAccount: null
+}
+
+const DEPOSIT = "DEPOSIT",
+      WITHDRAW = "WITHDRAW",
+      TRANSFER = "TRANSFER";
+
+
+function actionDeposit(amount){
+  return{
+    type: DEPOSIT,
+    amount 
+  }
+}
+
+function actionWithdraw(amount){
+  return {
+    type: WITHDRAW,
+    amount
+  }
+}
+
+function actionTransfer(amount, destinationAccount){
+  return {
+    type: TRANSFER,
+    amount,
+    destinationAccount
+  }
+}
+
